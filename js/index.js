e3a8533e28e5e7cb6a53d66f47e81cfa5e0c9cc9
ptatasu <garymason922@gmail.com>
2025-03-08 13:36:50 +0800

    front page done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const startBtn= document.getElementById("start");
 const styleBtn1 = document.getElementById("style_1");
 const styleBtn2 = document.getElementById("style_2");
+const sample1 = document.getElementById("sample1");
+const sample2 = document.getElementById("sample2");
+const cont = document.querySelector(".style-container");
 
 const Start = () => {
   startBtn.style.opacity = "0%";
@@ -9,6 +12,10 @@ const Start = () => {
   startBtn.style.transform = "translateY(-400px)";
   styleBtn1.style.opacity = "100%";
   styleBtn2.style.opacity = "100%";
+  sample1.style.opacity = "100%";
+  sample2.style.opacity = "100%";
+  cont.style.opacity = "100%";
+  cont.style.transform = "translateY(60px)";
   styleBtn1.style.cursor = "pointer";
   styleBtn2.style.cursor = "pointer";
 };
