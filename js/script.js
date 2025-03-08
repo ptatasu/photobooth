015749e42904534691919da53dbc13e5d086d9bd
ptatasu <garymason922@gmail.com>
2025-03-08 19:22:30 +0800

    cam done

--- js/script.js
+++ js/script.js
@@ -8,10 +8,8 @@ const image_1 = document.querySelector(".image_1");
 const image_2 = document.querySelector(".image_2");
 const image_3 = document.querySelector(".image_3");
 const image_4 = document.querySelector(".image_4");
-const image_1_1 = document.querySelector(".image_1_1");
-const image_2_2 = document.querySelector(".image_2_2");
-const image_3_3 = document.querySelector(".image_3_3");
-const image_4_4 = document.querySelector(".image_4_4");
+
+
 // const timeout = setTimeout(function(e){console.log(e)}, 3000);
 // const canvas = document.getElementById(canvas)
 let images = {};
@@ -27,58 +25,76 @@ const Load = () => {
   image_3.src = images[1];
   image_2.src = images[2];
   image_4.src = images[3];
-  image_1_1.src = images[0];
-  image_3_3.src = images[1];
-  image_2_2.src = images[2];
-  image_4_4.src = images[3];
 };
 
 const Capture = () => {
   window.location.assign("cam.html");
 };
 
-const Snapshot = (e) => {
-  // const interval = setInterval(function () {
-  //   console.log(e);
-  //   countdown.innerHTML = `${e}`;
-  //   e--;
-  //   if (e == -1) {
+const reset = () => {
+  snap.style.opacity = "100%";
+}
+
+const Snapshot = () => {
+  snap.onclick = 'null';
+  snap.style.opacity = "20%";
+  let time = document.getElementById("text").value;
+  countdown.innerHTML = `${time}`;
+  time--;
+  const interval = setInterval(function () {
+    console.log(time);
+  // console.log(time);
+    countdown.innerHTML = `${time}`;
+    time--;
+    if (time == -1) {
       canvas
         .getContext("2d")
         .drawImage(webcamVid, 0, 150, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
-      // clearInterval(interval);
+      clearInterval(interval);
       countdown.innerHTML = ``;
       let data_url = canvas.toDataURL("image/jpeg");
       images[count] = data_url;
       console.log(JSON.stringify(images));
       if (count == 0) {
         image_1.src = data_url;
+        image_1.style.border = "2px solid black"
+        setTimeout(() => {Snapshot()}, 2000);
+        // reset();
       } else if (count == 1) {
         image_2.src = data_url;
+        image_2.style.border = "2px solid black"
+        setTimeout(() => {Snapshot()}, 2000);
+        // reset();
       } else if (count == 2) {
         image_3.src = data_url;
+        image_3.style.border = "2px solid black"
+        setTimeout(() => {Snapshot()}, 2000);
+        // reset();
       } else if (count == 3) {
         image_4.src = data_url;
+        image_4.style.border = "2px solid black"
         nextBtn.style.display = "block";
+        snap.style.display = "none";
       }
       count++;
     }
-  // }, 1000);
-// };
+  }, 1000);
+  // setTimeout(reset(), 3000)
+};
+// snap.onclick = Snapshot();
 
 const retake = () => {};
 const next = () => {
-  window.location.assign("done.html"); //dito ko natapos Snapshot(3)
+  window.location.assign("done.html");
   // Save(JSON.stringify(images));
 };
-
 const Camera = () => {
   navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-      webcamVid.srcObject = stream;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+  .getUserMedia({ video: {facingMode: "user"} })
+  .then((stream) => {
+    webcamVid.srcObject = stream;
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 };
